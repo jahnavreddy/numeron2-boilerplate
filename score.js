cd22685a4ac30572ec11dbb5f3d6e1b3cc9ad5fa
jahnavreddy <jahnavredy12@gmail.com>
2023-11-07 10:53:11 +0530

    main

--- score.js
+++ score.js
@@ -1,1 +1,16 @@
 // Iteration 8: Making scoreboard functional
+
+console.log(window.location.search)
+
+
+let params = new URLSearchParams(document.location.search);
+let score = params.get("score")
+  
+
+let scoreboard = document.getElementById("score-board");
+scoreboard.textContent = score;
+
+var playagain = document.getElementById("play-again-button")
+playagain.onclick = () =>{
+  window.location.href =("./game.html")
+}
